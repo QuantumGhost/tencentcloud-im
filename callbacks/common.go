f620b9c7040dba4bc70e2ea31ff8f43a5d2cab92
QuantumGhost <obelisk.reg+github@gmail.com>
2019-01-09 14:22:31 +0800

    misc: Add constants for url parameter names, Add utility functions
    Add utility functions for creating CallbackCommonResponse

--- callbacks/common.go
+++ callbacks/common.go
@@ -1,7 +1,33 @@
 package callbacks
 
+const (
+	UrlParamCallbackCommand = "CallbackCommand"
+	UrlParamSdkAppid        = "SdkAppid"
+	UrlParamContentType     = "contenttype"
+	UrlParamClientIP        = "ClientIP"
+	UrlParamOptPlatform     = "OptPlatform"
+)
+
+const (
+	PlatformRESTAPI = "RESTAPI"
+	PlatformWeb     = "Web"
+	PlatformAndroid = "Android"
+	PlatformiOS     = "iOS"
+	PlatformWindows = "Windows"
+	PlatformMac     = "Mac"
+	PlatformUnkown  = "Unkown"
+)
+
 type CallbackCommonResponse struct {
 	ActionStatus string `json:"ActionStatus"`
 	ErrorInfo    string `json:"ErrorInfo"`
 	ErrorCode    int    `json:"ErrorCode"`
+}
+
+func ResponseOK() CallbackCommonResponse {
+	return CallbackCommonResponse{ActionStatus: "OK"}
+}
+
+func ResponseFail(code int, reason string) CallbackCommonResponse {
+	return CallbackCommonResponse{ActionStatus: "FAIL", ErrorCode: code, ErrorInfo: reason}
 }
